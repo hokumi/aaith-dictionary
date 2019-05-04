a46322d38268959dea11f8e5e8f654eb1c79c237
Ari <34664181+hokumi@users.noreply.github.com>
2019-05-04 18:55:28 -0300

    add words.json

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-
-function errorHandler(e) {
-    if(err) throw err;
-  }
-
-
 var url = "aaith-dictionary.xlsx";
 
 var req = new XMLHttpRequest();
@@ -18,43 +11,6 @@ req.onload = function() {
   var first_sheet_name = workbook.SheetNames[0];
   var worksheet = workbook.Sheets[first_sheet_name];
   var listOfWords = XLSX.utils.sheet_to_json(worksheet);
-
-
-
-
-  function onInitFs(fs) {
-
-    fs.root.getFile('words.json', {create: true}, function(fileEntry) {
-  
-      // Create a FileWriter object for our FileEntry (log.txt).
-      fileEntry.createWriter(function(fileWriter) {
-  
-        fileWriter.onwriteend = function(e) {
-          console.log('Write completed.');
-        };
-  
-        fileWriter.onerror = function(e) {
-          console.log('Write failed: ' + e.toString());
-        };
-  
-        // Create a new Blob and write it to log.txt.
-        var blob = new Blob(listOfWords, {type: 'text/plain'});
-  
-        fileWriter.write(blob);
-        window.open('words.json', '_blank')
-  
-      }, errorHandler);
-  
-    }, errorHandler);
-  
-  }
-  
-  window.requestFileSystem(window.TEMPORARY, 5*1024*1024, onInitFs, errorHandler);
-
-
-
-
-
 
   function display(){
     var mainDiv = document.getElementById('main');
